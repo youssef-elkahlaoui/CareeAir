bb728239536115fb9da51e576f98fa71bd711068
youssef-elkahlaoui <youssefelkahlaoui18@gmail.com>
2025-02-18 08:29:12 +0100

    nsiiit hhhh

---
+++ app/layout.js
@@ -0,0 +1,50 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "CareeAIr",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <a href="https://youssef-elkahlaoui.rf.gd/" target="_blank" rel="noopener noreferrer">
+                  <p> by Youssef El kahlaoui</p>
+                </a>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/hero.jsx
@@ -0,0 +1,81 @@
+"use client";
+import { TypeAnimation } from 'react-type-animation';
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const HeroSection = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-6xl text-bold tracking-tighter md:text-6xl lg:text-7xl xl:text-8xl animate-gradient font-display">
+            Your AI Powered Tool for
+            <br />
+            <TypeAnimation
+              sequence={[
+                'Professional Success',
+                1000,
+                'ATS-friendly CV Building',
+                1000,
+                'Interview Prepation',
+                1000,
+                'Career Growth',
+                1000,
+              ]}
+              speed={50}
+              style={{ fontSize: '1em' }}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
+          </p>
+        </div>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button variant="custom" size="lg" className=" px-8">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/image.png"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-3xl shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
