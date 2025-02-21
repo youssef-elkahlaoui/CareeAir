c5bb01ece1f45408f5876f9febe7f60340d8572b
youssef-elkahlaoui <youssefelkahlaoui18@gmail.com>
2025-02-21 09:49:45 +0100

    Add Link2 component and update footer with new logo; remove unused banner images

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,8 @@ import { join } from "@prisma/client/runtime/library";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
+import {Link2
+} from "lucide-react";
 
 export const metadata = {
   title: "CareeAIr",
@@ -34,16 +36,24 @@ export default function RootLayout({ children }) {
             disableTransitionOnChange
           >
             <Header />
-            <main className="min-h-screen">{children}
-            <Analytics />
-            <SpeedInsights />
+            <main className="min-h-screen">
+              {children}
+              <Analytics />
+              <SpeedInsights />
             </main>
             <Toaster richColors />
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <a href="https://youssef-elkahlaoui.rf.gd/" target="_blank" rel="noopener noreferrer">
-                  <p> by Youssef El kahlaoui</p>
+                <a
+                  href="https://youssef-elkahlaoui.rf.gd/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p>
+                    <Link2 className="inline" /> &nbsp; By Youssef
+                    El kahlaoui
+                  </p>
                 </a>
               </div>
             </footer>
